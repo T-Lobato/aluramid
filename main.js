47110a579303175483066f002b82748adf20490a
Thyago Lobato <thyagollobato@gmail.com>
2023-03-22 09:55:08 -0300

    Iterando em listas

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0;
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;// template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+}
